3a020c348bf8385247ffe97ec221f5eb4d261aed
Batsonbob <130487226+Batsonbob@users.noreply.github.com>
2023-04-12 13:11:24 +0000

    обновила titled selector textinput

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -7,9 +7,9 @@ const Select = styled.select`
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border-radius: 24px;
+    border: 1px solid #E2E8F0;
+    background-color: #F8FAFC;
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
@@ -19,7 +19,7 @@ const Select = styled.select`
 function TitledSelector({title, options}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className='caption'>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
