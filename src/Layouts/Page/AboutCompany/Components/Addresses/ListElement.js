5e9b73b4c40fa6a91fe0b076f3502305041cd110
Batsonbob <130487226+Batsonbob@users.noreply.github.com>
2023-04-12 11:25:20 +0000

    поменяла иконки

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -12,8 +12,8 @@ function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
             <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
+            <SecondaryButton minWidth='0'><IconOnlyButton icon={<EditIcon/>}/></SecondaryButton>
+            <SecondaryButton minWidth='0'><IconOnlyButton icon={<RemoveIcon/>}/></SecondaryButton>
         </div>
     );
 }
