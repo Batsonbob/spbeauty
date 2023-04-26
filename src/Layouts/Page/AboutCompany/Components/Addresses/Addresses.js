d7b057cd3bc844bd3949422fca5360e8a45b7b06
Batsonbob <130487226+Batsonbob@users.noreply.github.com>
2023-04-26 13:52:18 +0000

    обновила стили шрифтов

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,7 +4,7 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 justify-content-between col mt-4 my-padding my-border'>
-            <div className='title'>Адреса заведений</div>
+            <div className='title' style={{paddingBottom: "24px"}}>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3 flex-grow-1'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
